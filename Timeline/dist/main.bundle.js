1b13bc819b510699d503dcf92c2d30ae0a436e52
aditeyapandey <aditeyapandey@gmail.com>
2018-11-14 17:42:18 -0500

    New timeline app

--- Timeline/dist/main.bundle.js
+++ Timeline/dist/main.bundle.js
@@ -143,6 +143,11 @@ var AppComponent = (function () {
         this.currentQuestion = "";
         this.startTimeFirstTimeDefine = false;
         this.randomizedQuestionOrder = [];
+        this.showInstructions = true;
+        this.showSurvey = false;
+        this.byPassQuestion = false; // Need to set to false before goes into production
+        this.testingParameter = false; // if this is true then a hardcoded case will show for testing
+        this.userIdentifier = "" + Math.floor(Math.random() * 10000) + 1;
         this.userResponseFinalData = [];
     }
     AppComponent.prototype.ngOnInit = function () {
@@ -150,27 +155,31 @@ var AppComponent = (function () {
         //Step1 : a. Set user demographics information: Set the information about the user upfront, we need user demographics information
         // We might need this for the current evaluation, also needs some thought
         this.userResponseLocalVar = new __WEBPACK_IMPORTED_MODULE_2__models_userResponse_model__["a" /* UserResponse */]();
-        //Step 3: Initialize the time for the evaluation
-        this.data.getClock().subscribe(function (time) {
-            _this.time = time;
-            if (!_this.startTimeFirstTimeDefine) {
-                _this.startTime = time;
-                _this.startTimeFirstTimeDefine = true;
-            }
-        });
+        //Step 3: Initialize the time for the evaluation, needs to start at the end of instruction
+        // this.data.getClock().subscribe(time => {
+        //   this.time = time;
+        //   if(!this.startTimeFirstTimeDefine){
+        //     this.startTime = time;
+        //     this.startTimeFirstTimeDefine = true;
+        //   }
+        // });
         //Step 2: Load the questions from the database
         this.data.getQuestions().subscribe(function (response) {
             _this.questions = response;
-            //Test Step: Testing the randomization
-            _this.randomizedQuestionOrder = _this.testRandomization();
-            //Sometimes classes are statically compiled and need to be refreshed
-            // this.imgName=this.questions[0].file
-            // this.currentQuestion=this.questions[0].question
-            //Testing with randomized order
-            _this.imgName = _this.questions[_this.randomizedQuestionOrder[_this.currentQuestionCounter] - 1].file;
+            if (_this.testingParameter) {
+                _this.randomizedQuestionOrder = [3, 23, 11, 18, 5, 10, 24, 4, 16, 12, 17, 13, 2, 20, 6, 15, 19, 8, 21, 1, 7, 14, 9, 22];
+                _this.imgName = "index.png";
+            }
+            else {
+                //Normal Step: Testing the randomization
+                _this.randomizedQuestionOrder = _this.testRandomization();
+                //Loading Images with randomized order
+                _this.imgName = _this.questions[_this.randomizedQuestionOrder[_this.currentQuestionCounter] - 1].file;
+            }
+            //Question
             _this.currentQuestion = _this.questions[_this.randomizedQuestionOrder[_this.currentQuestionCounter] - 1].question;
             //Data to record at the starting of the evaluation
-            _this.userResponseLocalVar.name = "Aditeya";
+            _this.userResponseLocalVar.name = _this.userIdentifier;
             // //Step 5: Record the time and also do it when the the survey is initiated
             // this.userResponseLocalVar.time=this.time;
             //Step 6: Store the timelineQuestionId and also do it when the surey is initiated
@@ -211,7 +220,7 @@ var AppComponent = (function () {
     AppComponent.prototype.saveData = function () {
         //Initializing step
         this.userResponseLocalVar = new __WEBPACK_IMPORTED_MODULE_2__models_userResponse_model__["a" /* UserResponse */]();
-        this.userResponseLocalVar.name = "Aditeya";
+        this.userResponseLocalVar.name = this.userIdentifier;
         //Step 1: Record the time and also do it when the the survey is initiated
         this.userResponseLocalVar.endTime = this.time;
         this.userResponseLocalVar.startTime = this.startTime;
@@ -229,22 +238,30 @@ var AppComponent = (function () {
     AppComponent.prototype.downloadData = function () {
         //Code copied from StackOverflow
         var theJSON = JSON.stringify(this.userResponseFinalData);
-        var uri = "data:application/json;charset=UTF-8," + encodeURIComponent(theJSON);
+        var uri = "data:application/json;filename=filename.json,charset=UTF-8" + encodeURIComponent(theJSON);
         var a = document.createElement('a');
         a.href = uri;
         a.innerHTML = "Right-click and choose 'save as...'";
         document.body.appendChild(a);
     };
     AppComponent.prototype.testRandomization = function () {
+        var localCounter = 0;
+        var counterExceeded = false;
         var firstNumber = Math.ceil(Math.random() * Math.floor(24));
         var arrIndex = [firstNumber];
         var indexUsed = [firstNumber];
         for (var i = 1; i < 24; i++) {
             var checkCondition = true;
             while (checkCondition) {
+                localCounter += 1;
+                if (localCounter > 1000) {
+                    counterExceeded = true;
+                    break;
+                }
+                console.log(localCounter + 1);
                 var randomIndex = Math.ceil(Math.random() * Math.floor(24));
                 if (indexUsed.indexOf(randomIndex) == -1) {
-                    if (Math.abs(arrIndex[i - 1] - randomIndex) == 1 || Math.abs(arrIndex[i - 1] - randomIndex) == 6) {
+                    if (Math.abs(arrIndex[i - 1] - randomIndex) == 1 || Math.abs(arrIndex[i - 1] - randomIndex) == 8) {
                         //do nothing
                     }
                     else {
@@ -255,7 +272,29 @@ var AppComponent = (function () {
                 }
             }
         }
-        return (arrIndex);
+        if (counterExceeded) {
+            console.log([3, 23, 11, 18, 5, 10, 24, 4, 16, 12, 17, 13, 2, 20, 6, 15, 19, 8, 21, 1, 7, 14, 9, 22]);
+            return ([3, 23, 11, 18, 5, 10, 24, 4, 16, 12, 17, 13, 2, 20, 6, 15, 19, 8, 21, 1, 7, 14, 9, 22]);
+        }
+        else {
+            console.log(arrIndex);
+            return (arrIndex);
+        }
+    };
+    AppComponent.prototype.moveToSurvey = function () {
+        var _this = this;
+        console.log("move to survey");
+        this.showInstructions = false;
+        this.showSurvey = true;
+        //Moving the clock time to this point
+        this.data.getClock().subscribe(function (time) {
+            _this.time = time;
+            if (!_this.startTimeFirstTimeDefine) {
+                _this.startTime = time;
+                console.log(_this.startTime);
+                _this.startTimeFirstTimeDefine = true;
+            }
+        });
     };
     return AppComponent;
 }());
@@ -351,7 +390,7 @@ exports = module.exports = __webpack_require__(83)(false);
 
 
 // module
-exports.push([module.i, "body{\n  margin: 5%;\n}\n\n.experimentHeader{\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: center;\n      justify-content: center;\n}\n\n.experimentCounter{\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: start;\n      justify-content: flex-start;\n}\n\n\n\n.timelineVis{\n  padding-left:30%;\n  padding-right:30%;\n}\n\n.nextButton{\n  padding-top: 2%;\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: center;\n      justify-content: center;\n}\n\nspan{\n  font-size: 20px;\n}\n\n.nextButtonActive{\n  margin-left: 0.5%;\n  background-color: indianred; /* Green */\n  border: none;\n  color: white;\n  font-size: 20px;\n}\n\n.nextButtonInActive{\n  margin-left: 0.5%;\n  font-size: 20px;\n}\n\n\n", ""]);
+exports.push([module.i, "body{\n  margin: 5%;\n}\n\n.experimentHeader{\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: center;\n      justify-content: center;\n}\n\n.experimentCounter{\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: start;\n      justify-content: flex-start;\n}\n\n\n\n.timelineVis{\n  padding-left:30%;\n  padding-right:30%;\n}\n\n.nextButton{\n  padding-top: 2%;\n  display:-ms-flexbox;\n  display:flex;\n  -ms-flex-pack: center;\n      justify-content: center;\n}\n\nspan{\n  font-size: 20px;\n}\n\n.instructions{\n  margin: 2%;\n}\n.leaveInstruction{\n  display:inline-block;\n  float: right;\n  margin-left: 0.5%;\n  background-color: lightseagreen; /* Green */\n  border: none;\n  color: white;\n  font-size: 32px;\n\n}\n\n.nextButtonActive{\n  margin-left: 0.5%;\n  background-color: indianred; /* Green */\n  border: none;\n  color: white;\n  font-size: 20px;\n}\n\n.nextButtonInActive{\n  margin-left: 0.5%;\n  font-size: 20px;\n}\n\n\n", ""]);
 
 // exports
 
@@ -364,7 +403,7 @@ module.exports = module.exports.toString();
 /***/ 291:
 /***/ (function(module, exports) {
 
-module.exports = "\n\n<div class=\"experimentHeader\" id=\"expHeader\" >\n    <h1 >\n      {{title}}\n    </h1>\n</div>\n\n<div class=\"experimentCounter\" id=\"counter\" >\n\n  <h3>\n    Q{{currentQuestionCounter+1}}/{{totalQuestionCounter}}.  {{currentQuestion}}\n    <!--<p>The time is {{time}}</p>-->\n  </h3>\n</div>\n\n\n\n<div class=\"visualizationContainer\" [style.width.px]=\"width\"\n     [style.height.px]=\"height\" class=\"timelineVis\" id=\"tvViz\">\n\n  <img width=\"100%\" [src]=\"imgHome+imgName\">\n\n</div>\n\n<div class=\"nextButton\">\n\n  <span>Enter your answer:</span> <input style=\"margin-left: 0.5%\" [(ngModel)]=\"userInput\" (ngModelChange)=\"userInputChange()\">\n  <button [class.nextButtonInActive]=\"ifInputNotProvided\" [class.nextButtonActive]=\"!ifInputNotProvided\" id=\"nextQuestionButton\" title=\"Please enter your answer\" [disabled]=\"ifInputNotProvided\" (click)=\"loadNextQuestion()\"> Next </button>\n\n</div>\n"
+module.exports = "\n\n<div class=\"experimentHeader\" id=\"expHeader\" >\n    <h1 >\n      {{title}}\n    </h1>\n</div>\n\n<div class=\"instructions\" [hidden]=\"!showInstructions\"  >\n  <h3> Instructions</h3>\n  <p>Vis Lab HCI project group welcomes you to our timeline survey. A timeline is an abstract visual representation of temporal events. </p>\n\n  <p>In this survey we will ask you 24 questions which you have to answer with the use of a timeline visualizations. After you answer the question you can move to the next question. At the end of the survey we will ask you to rate your questions and ask you briefly about your experience throught the survey. </p>\n\n  <p> One of our team member will always be available to answer your questions and address your concerns. Your data will be completely anonymized with no reference back to you. </p>\n\n  <p> At this point if you have any questions, please ask our team member and we will be happy to help. Otherwise, please select Next to start the survey </p>\n  <button class=\"leaveInstruction\" (click)=\"moveToSurvey()\">Next</button>\n\n\n</div>\n\n<div class=\"SurveyContainer\" [hidden]=\"!showSurvey\">\n<div class=\"experimentCounter\" id=\"counter\" >\n\n  <h2>\n    Q{{currentQuestionCounter+1}}/{{totalQuestionCounter}}.  {{currentQuestion}}\n    <!--<p>The time is {{time}}</p>-->\n  </h2>\n</div>\n\n\n\n<div class=\"visualizationContainer\" [style.width.px]=\"width\"\n     [style.height.px]=\"height\" class=\"timelineVis\" id=\"tvViz\">\n\n  <img width=\"100%\" [src]=\"imgHome+imgName\">\n\n</div>\n\n<div class=\"nextButton\">\n\n  <span>Enter your answer:</span> <input style=\"margin-left: 0.5%\" [(ngModel)]=\"userInput\" (ngModelChange)=\"userInputChange()\">\n  <button [class.nextButtonInActive]=\"ifInputNotProvided\" [class.nextButtonActive]=\"!ifInputNotProvided \" id=\"nextQuestionButton\" title=\"Please enter your answer\" [disabled]=\"ifInputNotProvided \" (click)=\"loadNextQuestion()\"> Next </button>\n\n</div>\n</div>\n"
 
 /***/ }),
 
